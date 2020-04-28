5a5d1070f86a04b224bb43b191cd0f880d782683
SirPotatoIV <otoolej33@gmail.com>
2020-04-28 15:33:06 -0500

    Created new component called Search Params. Removed Pet component from App.js and added Search Params  component.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,14 @@
 import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
-  // return React.createElement("div", {}, [
-  //   React.createElement("h1", {}, "Adopt Me!"),
-  //   React.createElement(Pet, {
-  //     name: "Luna",
-  //     animal: "Dog",
-  //     breed: "Havenese",
-  //   }),
-  //   React.createElement(Pet, { name: "Penny", animal: "Dog", breed: "Mutt" }),
-  //   React.createElement(Pet, { name: "Bowie", animal: "Cat", breed: "Mixed" }),
-  // ]);
-
   return (
     <div>
-      <h1>Adopt Me!</h1>
-      <Pet name="Penny" animal="Dog" breed="Mutt" />
-      <Pet name="Bowie" animal="Cat" breed="Mixed" />
-      <Pet name="Tucker" animal="Dog" breed="Doddle" />
-      <Pet name="Bindi" animal="Dog" breed="Australian Sheppard" />
+      <h1 id="something-important">Adopt Me!</h1>
+      <SearchParams />
     </div>
   );
 };
 
-render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
