f9af31527bb0ead213fa465d2254ca8a652d87c6
SirPotatoIV <otoolej33@gmail.com>
2020-04-28 15:01:42 -0500

    Changed over Pet component to JSX in previous commit. Changed App.js to JSX in this commit.

--- src/App.js
+++ src/App.js
@@ -3,16 +3,26 @@ import { render } from "react-dom";
 import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "Havenese",
-    }),
-    React.createElement(Pet, { name: "Penny", animal: "Dog", breed: "Mutt" }),
-    React.createElement(Pet, { name: "Bowie", animal: "Cat", breed: "Mixed" }),
-  ]);
+  // return React.createElement("div", {}, [
+  //   React.createElement("h1", {}, "Adopt Me!"),
+  //   React.createElement(Pet, {
+  //     name: "Luna",
+  //     animal: "Dog",
+  //     breed: "Havenese",
+  //   }),
+  //   React.createElement(Pet, { name: "Penny", animal: "Dog", breed: "Mutt" }),
+  //   React.createElement(Pet, { name: "Bowie", animal: "Cat", breed: "Mixed" }),
+  // ]);
+
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="Penny" animal="Dog" breed="Mutt" />
+      <Pet name="Bowie" animal="Cat" breed="Mixed" />
+      <Pet name="Tucker" animal="Dog" breed="Doddle" />
+      <Pet name="Bindi" animal="Dog" breed="Australian Sheppard" />
+    </div>
+  );
 };
 
 render(React.createElement(App), document.getElementById("root"));
