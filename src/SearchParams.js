0c62a24c0e184efb02dba1f3b32f8e6aceb61ea1
SirPotatoIV <otoolej33@gmail.com>
2020-05-01 10:31:01 -0500

    Added to package json to tell babel which browsers to be concerned about. Added function to request pets from API based on user inputs. Currently just console logging the returend pets.

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,18 +1,43 @@
-import React, { useState } from "react";
-import { ANIMALS } from "@frontendmasters/pet";
+import React, { useState, useEffect } from "react";
+import pet, { ANIMALS } from "@frontendmasters/pet";
 import useDropdown from "./useDropdown";
 
 const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA");
   const [breeds, setBreeds] = useState([]);
   const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
-  const [breed, BreedDropdown] = useDropdown("Breed", "", breeds);
-  // const [animal, setAnimal] = useState("dog");
-  // const [breed, setBreed] = useState("");
+  const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
+  const [pets, setPets] = useState([]);
+
+  async function requestPets() {
+    const { animals } = await pet.animals({
+      location,
+      breed,
+      type: animal,
+    });
+
+    setPets(animals || []);
+    console.log(pets);
+  }
+
+  useEffect(() => {
+    setBreeds([]);
+    setBreed("");
+
+    pet.breeds(animal).then(({ breeds: apiBreeds }) => {
+      const breedStrings = apiBreeds.map(({ name: breedName }) => breedName);
+      setBreeds(breedStrings);
+    }, console.error);
+  }, [animal, setBreeds, setBreed]);
 
   return (
     <div className="search-params">
-      <form>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          requestPets();
+        }}
+      >
         <label htmlFor="location">
           location
           <input
--- src/useDropdown.js
+++ src/useDropdown.js
@@ -4,7 +4,7 @@ const useDropdown = (label, defaultState, options) => {
   const [state, setState] = useState(defaultState);
   const id = `use-dropdown-${label.replace(" ", "").toLowerCase()}`;
   const Dropdown = () => (
-    <label htmFor={id}>
+    <label htmlFor={id}>
       {label}
       <select
         id={id}
